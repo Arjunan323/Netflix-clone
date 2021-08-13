776d677d566feea12d529c92a53901776a5bbbe5
Arjunan323 <uarjunan97@gmail.com>
2021-08-13 16:27:42 +0530

    spring starter template

---
+++ netflix-clone-server/src/main/java/com/arjunan/netflixcloneserver/NetflixCloneServerApplication.java
@@ -0,0 +1,13 @@
+package com.arjunan.netflixcloneserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetflixCloneServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixCloneServerApplication.class, args);
+	}
+
+}
---
+++ netflix-clone-server/src/test/java/com/arjunan/netflixcloneserver/NetflixCloneServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arjunan.netflixcloneserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixCloneServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
